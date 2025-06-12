281977b0a52ade3c00b81752ae6db86cc15f03fd
Gustavo <gustavosardinha643@gmail.com>
2025-06-11 22:56:16 -0300

    Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -892,6 +892,7 @@ function Reconstrucao({initialState}: HomeWrapperProps){
   const [esps, setEsps] = useState<number[]>(initialState?.esps || []);
   const [solution_consts, setSolutionsConst] = useState<number[]>([]);
   const [filterPoint, setFilterPoint] = useState<string>("");
+  const [filterInterval, setFilterInterval] = useState<string>("");
 
   useEffect(() => {
     if(solution_consts.length === 0)
@@ -925,10 +926,8 @@ function Reconstrucao({initialState}: HomeWrapperProps){
 
     setSolutionsConst(sol_const);
   }
-  
-  function fluxFunction(x: number): number {
-    if (!result) return -1;
-    
+  function findAPoint(x: number): number[]{
+    if (!result) return [];
     const {
       numRegioes,
       mapeamento,
@@ -951,20 +950,64 @@ function Reconstrucao({initialState}: HomeWrapperProps){
           }
         }
       }
+      return [numReg, size];
+  }
+  return [];
+}
+  function fluxFunction(x: number): number {
+    if (!result) return -1;
+    
+    const {
+      numRegioes,
+      mapeamento,
+      fonteNeutrons,
+      coeficientesDifusao,
+      choquesMacroscopicos,
+      espessura,
+      comprimento,
+    } = result;
+    const regInfos = findAPoint(x);
       let dx = x;
-      if(espessura[numReg] != null)
-        dx = x - (size - espessura[numReg]);
-      console.log(size)
-      console.log(espessura[numReg])
+      if(espessura[regInfos[0]] != null)
+        dx = x - (regInfos[1] - espessura[regInfos[0]]);
+      console.log(regInfos[1])
+      console.log(espessura[regInfos[0]])
       console.log(dx)
-      const zona = mapeamento[numReg];
+      const zona = mapeamento[regInfos[0]];
       const L = Math.sqrt(coeficientesDifusao[zona - 1]/choquesMacroscopicos[zona - 1]);
-      const flux = solution_consts[2*numReg]*Math.pow(Math.E, dx/L) + solution_consts[2*numReg + 1]*Math.pow(Math.E, -dx/L) + fonteNeutrons[numReg]/choquesMacroscopicos[zona - 1];
+      const flux = solution_consts[2*regInfos[0]]*Math.pow(Math.E, dx/L) + solution_consts[2*regInfos[0] + 1]*Math.pow(Math.E, -dx/L) + fonteNeutrons[regInfos[0]]/choquesMacroscopicos[zona - 1];
       return flux;
     }
-    return -1;
+  function absorptionRate(a: number, b:number) : number{
+    if (!result) return -1;
+    const {
+      numRegioes,
+      mapeamento,
+      fonteNeutrons,
+      coeficientesDifusao,
+      choquesMacroscopicos,
+      espessura,
+      comprimento,
+    } = result;
+    const regInfosA = findAPoint(a);
+    const regInfosB = findAPoint(b);
+    
+    let absRate = 0;
+    for(let i = regInfosA[0]; i <= regInfosB[0]; i++){
+      const zona = mapeamento[i];
+      const L = Math.sqrt(coeficientesDifusao[zona - 1]/choquesMacroscopicos[zona - 1]);
+      let dxa = 0;
+      let dxb = espessura[i];
+      if(i == regInfosA[0])
+        dxa = a - regInfosA[1];
+      if(i == regInfosB[0])
+        dxb = b - regInfosB[1];
+      console.log(dxa);
+      console.log(dxb);
+      absRate += L*choquesMacroscopicos[zona - 1]*(solution_consts[2*i]*(Math.pow(Math.E, dxb/L) - Math.pow(Math.E, dxa/L)) - solution_consts[2*i + 1]*(Math.pow(Math.E, - dxb/L) - Math.pow(Math.E, - dxa/L))) + fonteNeutrons[i]*(dxb - dxa)
+    }
+    return absRate;
   }
-  
   return(
     <div>
       <ArrayFormInput
@@ -981,6 +1024,19 @@ function Reconstrucao({initialState}: HomeWrapperProps){
         {(fluxFunction(Number(filterPoint)) >= 0) && (
           <p style={{textAlign: 'center'}}>O valor em {filterPoint} é {fluxFunction(Number(filterPoint))}</p>
         )}
+      </div>
+
+      <ArrayFormInput
+      label='Digite um intervalo'
+      placeholder='Informe o intervalo para calcular a taxa de absorção'
+      value={filterInterval}
+      msgAlert='Informe um intervalo do domínio para calcular a taxa de absorção'
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => setFilterInterval(event.target.value)}
+      ></ArrayFormInput>
+      <div style={{justifyContent: 'center', alignItems: 'center'}}>
+        {( (filterInterval.split(";").length == 2) && (Number(filterInterval.split(";")[0]) >= 0) && (Number(filterInterval.split(";")[1]) >= 0)) && (
+          <p style={{textAlign: 'center'}}>O valor em {filterInterval} é {absorptionRate(Number(filterInterval.split(";")[0]), Number(filterInterval.split(";")[1]))}</p>
+        )}  
       </div>
     </div>
   );
